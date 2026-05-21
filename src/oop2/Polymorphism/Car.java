9669b1df849967f2ecaefd01789f7daa1adefe56
iamhimangshude <dehimangshu03@gmail.com>
2026-05-21 20:08:54 +0530

    Completed the Challenge in Java related to polymorphism and overriding of methods.

--- src/oop2/Polymorphism/Car.java
+++ src/oop2/Polymorphism/Car.java
@@ -24,11 +24,43 @@ public class Car {
 class GasPoweredCar extends Car{
     private double avgKmPerLitre;
     private int cylinders;
+
+    public GasPoweredCar(String description, double avgKmPerLitre, int cylinders) {
+        super(description);
+        this.avgKmPerLitre = avgKmPerLitre;
+        this.cylinders = cylinders;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("Gas -> All %d cylinders are fired up, Ready!%n", cylinders);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("Gas -> usage exceeds the average %.2f %n", avgKmPerLitre);
+    }
 }
 
-class ElectricPoweredCar extends  Car{
+class ElectricCar extends  Car{
     private double avgKmPerCharge;
     private int batterySize;
+
+    public ElectricCar(String description, int batterySize, double avgKmPerCharge) {
+        super(description);
+        this.batterySize = batterySize;
+        this.avgKmPerCharge = avgKmPerCharge;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("BEV -> switch %d kWh battery on, Ready! %n", batterySize);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("BEV -> usage under the average: %.2f %n", avgKmPerCharge);
+    }
 }
 
 class HybridCar extends Car{
@@ -36,4 +68,21 @@ class HybridCar extends Car{
     private int batterySize;
     private int cylinders;
 
+    public HybridCar(String description, double avgKmPerLitre, int batterySize, int cylinders) {
+        super(description);
+        this.avgKmPerLitre = avgKmPerLitre;
+        this.batterySize = batterySize;
+        this.cylinders = cylinders;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("Hybrid -> %d cylinders are fired up.%n", cylinders);
+        System.out.printf("Hybrid -> switch %d kWh battery on, Ready!%n", batterySize);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("Hybrid -> usage below average: %.2f %n", avgKmPerLitre);
+    }
 }
--- src/oop2/Polymorphism/CarMain.java
+++ src/oop2/Polymorphism/CarMain.java
@@ -3,7 +3,17 @@ package oop2.Polymorphism;
 
 public class CarMain {
     public static void main(String[] args) {
+        Car car = new Car("2022 Blue Ferrari 296 GTS");
+        runRace(car);
 
+        Car ferrari = new GasPoweredCar("2022 Blue Ferrari 296 GTS", 15.4, 6);
+        runRace(ferrari);
+
+        Car tesla = new ElectricCar("2022 Red Tesla Model 3 EV Vehicle", 75,568);
+        runRace(tesla);
+
+        Car ferrariHybrid = new HybridCar("2022 Black Ferrari SF90 Stradale", 16, 8, 8);
+        runRace(ferrariHybrid);
     }
 
     public static void runRace(Car car){
