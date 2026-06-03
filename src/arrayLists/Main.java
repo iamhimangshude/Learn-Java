28a731186a09a413ffe31d38a3079bbd0c97848a
iamhimangshude <dehimangshu03@gmail.com>
2026-06-03 21:45:31 +0530

    added java files
    - learnt about List interface and its methods
    - learnt about ArrayList and its methods (especially sorting, searching, copying, adding elements, removing and many others).

---
+++ src/arrayLists/Main.java
@@ -0,0 +1,42 @@
+package arrayLists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// REVISITED KNOWLEDGE: `record`s in java creates immutable objects. best for data transfer operations
+record GroceryItem(String name, String type, int count){
+    public GroceryItem(String name){
+        this(name, "DAIRY", 1);
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%d %s in %s", count, name.toUpperCase(), type);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        GroceryItem[] groceryArray = new GroceryItem[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+        groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
+        System.out.println(Arrays.toString(groceryArray));
+
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("yogurt");
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter"));
+        groceryList.add(new GroceryItem("Milk"));
+        groceryList.add(new GroceryItem("oranges", "PRODUCE", 5));
+        System.out.println(groceryList);
+        groceryList.set(0, new GroceryItem("apples", "PRODUCE", 6));
+        System.out.println(groceryList);
+        groceryList.remove(1);
+        System.out.println(groceryList);
+
+    }
+}
---
+++ src/arrayLists/MoreLists.java
@@ -0,0 +1,65 @@
+package arrayLists;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MoreLists {
+    public static void main(String[] args) {
+        String[] items = { "apples", "bananas", "milk", "eggs" };
+
+        // Lists are immutable data type in Java
+        List<String> list = List.of(items);
+        System.out.println(list);
+        ArrayList<String> groceries = new ArrayList<>(list);
+        groceries.add("yogurt");
+        System.out.println(groceries);
+
+        // All in one statement
+        ArrayList<String> nextList = new ArrayList<>(List.of("pickles", "mustard", "cheese"));
+        System.out.println(nextList);
+        groceries.addAll(nextList);
+        System.out.println(groceries);
+
+        System.out.println("third item = " + groceries.get(2));
+
+        if (groceries.contains("mustard")) {
+            System.out.println("list contains mustard");
+        }
+
+        groceries.add("yogurt");
+        System.out.println("first = " + groceries.indexOf("yogurt"));
+        System.out.println("last = " + groceries.lastIndexOf("yogurt"));
+
+        System.out.println(groceries);
+        groceries.remove(1);
+        System.out.println(groceries);
+        groceries.remove("yogurt");
+        System.out.println(groceries);
+
+        groceries.removeAll(List.of("apples", "eggs"));
+        System.out.println(groceries);
+
+        groceries.retainAll(List.of("apples", "milk", "mustard", "cheese"));
+        System.out.println(groceries);
+
+        groceries.clear();
+        System.out.println(groceries);
+
+        System.out.println("Is empty = " + groceries.isEmpty());
+
+        groceries.addAll(List.of("apples", "milk", "mustard", "cheese"));
+        groceries.addAll(Arrays.asList("eggs", "pickles", "mustard", "ham"));
+
+        System.out.println(groceries);
+        groceries.sort(Comparator.naturalOrder());
+        System.out.println(groceries);
+
+        groceries.sort(Comparator.reverseOrder());
+        System.out.println(groceries);
+
+        var groceryArray = groceries.toArray(new String[groceries.size()]);
+        System.out.println(Arrays.toString(groceryArray));
+    }
+}
