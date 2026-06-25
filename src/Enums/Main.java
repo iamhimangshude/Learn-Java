611766a3e7633f18f80ddd4755b1a908df876629
iamhimangshude <dehimangshu03@gmail.com>
2026-06-25 08:26:51 +0530

    Added enums concept part 2 files

--- src/Enums/Main.java
+++ src/Enums/Main.java
@@ -9,12 +9,26 @@ public class Main {
 
         for (int i = 0; i < 10; i++) {
             weekDay = getRandomDay();
-            System.out.printf("Name is %s, Ordinal value = %d%n",
-                    weekDay.name(), weekDay.ordinal());
+//            System.out.printf("Name is %s, Ordinal value = %d%n",
+//                    weekDay.name(), weekDay.ordinal());
+//
+//            if (DayOfTheWeek.FRI == weekDay) {
+//                System.out.println("Found a FRIDAY!!!");
+//            }
+            switchDayOfTheWeek(weekDay);
+        }
+        for (Topping topping : Topping.values()){
+            System.out.println(topping.name() + " : " + topping.getPrice());
+        }
+    }
 
-            if (DayOfTheWeek.FRI == weekDay){
-                System.out.println("Found a FRIDAY!!!");
-            }
+    private static void switchDayOfTheWeek(DayOfTheWeek weekDay) {
+        int weekDayInteger = weekDay.ordinal() + 1;
+        switch (weekDay) {
+            case WED -> System.out.println("Wednesday is Day " + weekDayInteger);
+            case SAT -> System.out.println("Saturday is Day " + weekDayInteger);
+            default ->
+                    System.out.println(weekDay.name().charAt(0) + weekDay.name().substring(1).toLowerCase() + "day is Day" + weekDayInteger);
         }
     }
 
