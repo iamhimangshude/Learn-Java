5cf5c3632890b7199effbfff3f30be1e468b138f
iamhimangshude <dehimangshu2020@outlook.com>
2026-09-11 21:16:45 +0530

    init: added java files for data types and hello world in java

---
+++ src/DTypes.java
@@ -0,0 +1,20 @@
+public class DTypes {
+    public static void main(String[] args) {
+        // Integer types:
+        int in = 10;
+        byte by = 127;
+        short sh = 558;
+        long l = 5458l;
+
+        // Decimal types:
+        float marks = 6.5f;
+        double d = 3.142857;
+
+        // Character type:
+        char c = 'k'; // NOT "K"
+
+        // Boolean type:
+        boolean b = true;
+
+    }
+}
